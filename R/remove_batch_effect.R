8397513428f437d7e9a7e48dd6a7f92c4f98889f
qwerty239qwe <qwerty239qwe@gmail.com>
2022-09-07 15:47:43 +0800

    mod batch effect removing code

--- R/remove_batch_effect.R
+++ R/remove_batch_effect.R
@@ -1,3 +1,22 @@
+#!/usr/bin/env Rscript
+
+'Do enrichment analysis using raw count data
+Usage:
+    remove_batch_effect.R [--method=<method> --group] <input> <metadata> <output>
+    
+Options:
+    -h --help  Show this screen.
+    -v --version  Show version.
+    --method=<method>  The name of p-value adjusting method [default: combatseq]
+    --group  Consider group column in the metadata
+
+Arguments:
+    input  input csv file name
+    metadata  metadata file name
+    output  output folder
+' -> doc
+
+
 library(biomaRt)
 library(glue)
 library(sva)
@@ -5,9 +24,9 @@ library(RUVSeq)
 library(limma)
 library(readxl)
 library(here)
-library(ggplot2)
+suppressMessages(library(docopt))
 
-cleanY = function(y, mod, svs) {
+cleanY <- function(y, mod, svs) {
   X = cbind(mod, svs)
   Hat = solve(t(X) %*% X) %*% t(X)
   beta = (Hat %*% t(y))
@@ -17,53 +36,50 @@ cleanY = function(y, mod, svs) {
   return(y - t(as.matrix(X[,-c(1:P)]) %*% beta[-c(1:P),]))
 }
 
-root_dir <- file.path("../../DOX/data/merged/wgcna_4/")
-df <- read.csv(file.path(root_dir, "raw_count.csv"), row.names = 1)
+args <- docopt(doc)
+df <- read.csv(file.path(args$input), row.names = 1)
 df <- df[rowSums(df) != 0,]
 
-meta <- read.csv(file.path(root_dir, "meta.csv"), row.names = 1)
-meta <- as.data.frame(readxl::read_excel(file.path(root_dir, "labels_may_mg.xlsx")))
-batch <- paste(batch, meta$condition)
-group <- meta$dosage
-mod <- model.matrix(~group)
-mod0 <- cbind(mod[,1])
-controls <- (group == 1)  # for supervised methods
+meta <- read.csv(args$metadata, row.names = 1)
+batch <- meta$batch
+# batch <- paste(batch, meta$condition)
+if (args$group){
+  group <- meta$group
+  mod <- model.matrix(~group)
+  mod0 <- cbind(mod[,1])
+}
 
-# PCA
-pca_res <- prcomp(t(df), scale. = TRUE)
-autoplot(pca_res, data=meta, colour = 'group')
 
-# limma
-limma_fit <- limma::removeBatchEffect(x=log(as.matrix(df) + 1), batch = factor(batch))
-pca_res <- prcomp(t(limma_fit), scale. = TRUE)
-autoplot(pca_res, data=meta, colour = 'group')
-write.csv(exp(limma_fit) - 1, file.path(root_dir, "mg_count_jun_br_limma.csv"))
+if (tolower(args$method) == "combatseq") {
+  if (args$group){
+    adjusted <- sva::ComBat_seq(as.matrix(df), batch=batch, group=group)
+  } else {
+    adjusted <- sva::ComBat_seq(as.matrix(df), batch=batch)
+  }
+} else if (tolower(args$method) == "limma") {
+  limma_fit <- limma::removeBatchEffect(x=log(as.matrix(df) + 1), batch = factor(batch))
+  adjusted <- exp(limma_fit) - 1
+} else if (method == "ruv") { 
+  # not finished
+  controls <- (group == 1)
+  ruvfit <- RUVg(df, cIdx= controls, k=1)
+  batch_ruv_cp <- ruvfit$W
+} else {
+  stop("Invalid method")
+}
 
-# ruv
-ruvfit <- RUVg(df, cIdx= controls, k=1)
-batch_ruv_cp <- ruvfit$W
+write.csv(adjusted, args$output)
 
 # sva
-svafit <- sva::svaseq(as.matrix(df), mod = mod, mod0=mod0, n.sv=2)
-batch_sva_us <- svafit$sv
+# controls <- (group == 1)  # for supervised methods
+# svafit <- sva::svaseq(as.matrix(df), mod = mod, mod0=mod0, n.sv=2)
+# batch_sva_us <- svafit$sv
 
 # svafit <- sva::svaseq(as.matrix(df), mod = mod, mod0=mod0, controls = controls, n.sv=2)
 # batch_sva_s <- svafit$sv
-
-plot(batch_sva_us, col=factor(batch), cex=1,
-     xlab="SV1", ylab="SV2")
+# 
+# plot(batch_sva_us, col=factor(batch), cex=1,
+#      xlab="SV1", ylab="SV2")
 
 # combat
 # sva::ComBat()
-
-
-adjusted <- sva::ComBat_seq(as.matrix(df), batch=batch)
-pca_res <- prcomp(t(adjusted), scale. = TRUE)
-autoplot(pca_res, data=meta, colour = 'dosage')
-write.csv(adjusted, file.path(root_dir, "count_br.csv"))
-
-
-limma_fit <- limma::removeBatchEffect(x=log(as.matrix(adjusted) + 1), batch = factor(batch))
-write.csv(exp(limma_fit) - 1, file.path(root_dir, "mg_count_jun_br_combatseq_br_limma.csv"))
-
-limma_fit
