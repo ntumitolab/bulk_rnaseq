b1fcc9193a83b2d54e34ae476a2f19e16f0dbfe5
qwerty239qwe <qwerty239qwe@gmail.com>
2022-12-08 01:56:09 +0800

    add new arg

--- R/enrichment_main.r
+++ R/enrichment_main.r
@@ -2,7 +2,7 @@
 
 'Do enrichment analysis using raw count data
 Usage:
-    enrichment_main.R (--enrich | --gsea) [--plot --list --dePval=<dePval> --pval=<pval> --qval=<qval> --adjMethod=<adjMethod> --geneName=<geneName>] <input> <output>
+    enrichment_main.R (--enrich | --gsea) [--plot --list --dePval=<dePval> --deLFC=<deLFC> --pval=<pval> --qval=<qval> --adjMethod=<adjMethod> --geneName=<geneName>] <input> <output>
     
 Options:
     -h --help  Show this screen.
@@ -12,6 +12,7 @@ Options:
     --gsea  Perform GSEA
     --list  If specified, the input data is treated as a list of genes, otherwise it will be treated as a DEG table
     --dePval=<dePval>  The p-value used to select DEGs [default: 0.05]
+    --deLFC=<deLFC>  The log2FC used to select DEGs [default: 1]
     --pval=<pval>  The p-value cutoff used to run the analysis [default: 0.05]
     --qval=<qval>  The q-value cutoff used to run the analysis [default: 0.2]
     --adjMethod=<adjMethod>  The name of p-value adjusting method [default: BH]
@@ -41,7 +42,7 @@ main <- function(args){
   } else {
     print("Using DEG table files")
     comparisons <- gsub("^DE_|.tsv", "", grep("^DE_", list.files(path = args$input), value = TRUE))
-    sapply(comparisons, split_DE, inDir=args$input, pvalthres=as.numeric(args$dePval))
+    sapply(comparisons, split_DE, inDir=args$input, log2thres=as.numeric(args$deLFC), pvalthres=as.numeric(args$dePval))
   }
   if (args$enrich) {
     for (db in c("go", "kegg", "reactome")) {
